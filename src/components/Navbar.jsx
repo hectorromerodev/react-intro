635a20eefd0075cf0f4f369a389ec89f2221f4a0
Hector Romero <hectorromerodev@gmail.com>
2022-11-24 21:49:40 -0800

    add Link routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,59 +6,66 @@ import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Button from "@mui/material/Button";
 
+import { Link } from "react-router-dom";
+
 import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCardIcon from "@mui/icons-material/ShoppingCart";
 
-const Navbar = ({setIsAdminVisible}) => {
+const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              textDecoration: "none",
-              color: "inherit",
-              display: { xs: "none", md: "flex" },
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow:1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              color="inherit"
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                textDecoration: "none",
+                color: "inherit",
+                display: { xs: "none", md: "flex" },
+              }}
             >
-              <MenuIcon />
-            </IconButton>
-          </Box>
-          <Box sx={{ flexGrow:1, display: { xs: "none", md: "flex" } }}>
-            <Button
-              onClick={() => setIsAdminVisible(true)}
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Admin
-            </Button>
-            <Button sx={{ my: 2, color: "white", display: "block" }}>
-              Shop
-            </Button>
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <IconButton sx={{ p: 0 }}>
-              <ShoppingCardIcon sx={{ color: "white" }} />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow:1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box sx={{ flexGrow:1, display: { xs: "none", md: "flex" } }}>
+              <Link to="/admin" style={{textDecoration: 'none'}}>
+                <Button
+                  sx={{ my: 2, color: "white", display: "block" }}
+                  children="Admin"
+                /> 
+              </Link>
+              <Link to="/shop" style={{textDecoration: 'none'}}>
+                <Button
+                  sx={{ my: 2, color: "white", display: "block" }}
+                  children="Shop"
+                />
+              </Link>
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <IconButton sx={{ p: 0 }}>
+                <ShoppingCardIcon sx={{ color: "white" }} />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 };
 
