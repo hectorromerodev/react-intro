83b22d3970c14b0a44265fdab0dae324428e5ac5
Hector Romero <hectorromerodev@gmail.com>
2022-11-24 22:10:35 -0800

    update admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -6,72 +6,75 @@ import {
 
 import AddCircleOutline from '@mui/icons-material/AddCircleOutline';
 
-import AddProductModal from '../modals/ProductModal';
-import ProductItemCard from '../components/ProductItemCard';
+import AddProductModal from '../modals/AddProductModal';
+import EditProductModal from '../modals/EditProductModal';
+import ProductList from '../components/ProductList';
 
-const AdminPage = ({ allProducts, setAllProducts}) => {
-  const [isAddProductVisible, setIsAddProductVisible] = useState(false);
+const AdminPage = ({ allProducts, setAllProducts }) => {
   
+  const [isAddProductVisible, setIsAddProductVisible] = useState(false);
+  const [isEditProductVisible, setIsEditProductVisible] = useState(false);
+  const [productToEdit, setProductToEdit] = useState();
+
   const handleOnSubmit = product => {
     setAllProducts([...allProducts, product]);
+  }
+
+  const handleOnEdit = product => {
+    const index = allProducts.findIndex(p => p.id === product.id);
+    allProducts[index] = product;
+    setAllProducts([...allProducts]);
   }
 
   const handleOnDelete = product => {
     setAllProducts(allProducts.filter(p => p !== product));
   }
 
-  const handleOnEdit = product => {
-    console.log('Edit', product);
-  }
-
   return (
-    <Box>
-      <Fab
-        variant="extended"
-        onClick={() => setIsAddProductVisible(true)}
-        sx={{
-          position: 'absolute',
-          bottom: '24px',
-          right: '24px',
-        }}
-      >
-        <AddCircleOutline
+    <>
+      <Box>
+        <Fab
+          variant="extended"
+          onClick={() => setIsAddProductVisible(true)}
           sx={{
-            mr: 1,
+            position: 'absolute',
+            bottom: '24px',
+            right: '24px',
+          }} 
+        >
+          <AddCircleOutline
+            sx={{
+              mr: 1,
+            }}
+          />
+          Add a new product
+        </Fab>
+        
+        <ProductList
+          variant="admin"
+          allProducts={allProducts}
+          onDelete={handleOnDelete}
+          onEdit={(product) => {
+            console.log('product', product);
+            setProductToEdit(product);
+            setIsEditProductVisible(true);
           }}
         />
-        Add a new product
-      </Fab>
-      
-      {
-        <Box sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-          gap: '1rem',
-          padding: '1rem',
-        }}>
-          {
-            allProducts.map(product => (
-              <Box key={product.id}>
-                <ProductItemCard
-                  product={product}
-                  onDelete={handleOnDelete}
-                  onEdit={handleOnEdit}
-                />
-              </Box>
-            ))
 
-          }
-        </Box>
-      }
+        <AddProductModal
+          isOpen={isAddProductVisible}
+          onClose={() => setIsAddProductVisible(false)}
+          onSubmit={handleOnSubmit}
+        />
 
-      
-      <AddProductModal
-        open={isAddProductVisible}
-        onClose={() => setIsAddProductVisible(false)}
-        onSubmit={handleOnSubmit}
-      />
-    </Box>
+        <EditProductModal
+          isOpen={isEditProductVisible}
+          productToEdit={productToEdit}
+          onClose={() => setIsEditProductVisible(false)}
+          onSubmit={handleOnEdit}
+        />
+      </Box>
+    </>
   )
 };
 
