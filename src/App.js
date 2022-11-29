4af4ef5c1914885b07b6befdb4d8918aedcbc871
Hector Romero <hectorromerodev@gmail.com>
2022-11-29 15:26:22 -0800

    add mock products data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import AdminPage from './pages/AdminPage';
 import ShopPage from './pages/ShopPage';
+import ShopCartPage from './pages/ShopCartPage';
+
+// Data
+import products from './data/products';
 
 function App() {
 
   
-  const [allProducts, setAllProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState(products);
 
   return (
     <>
@@ -16,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/admin" element={<AdminPage allProducts={allProducts} setAllProducts={setAllProducts} />} />
         <Route path="/shop" element={<ShopPage allProducts={allProducts} />} />
+        <Route path="/shop-cart" element={<ShopCartPage />} />
       </Routes>
     </>
 
