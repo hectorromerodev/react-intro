fe7f37b5af364a6abfae8efaf84a4db37ea348b6
Hector Romero <hectorromerodev@gmail.com>
2022-12-05 14:06:38 -0800

    update products lists

--- src/data/products.js
+++ src/data/products.js
@@ -1,40 +1,49 @@
-import { v4 as uuidv4 } from 'uuid';
 
 const products = [
     {
-        id: uuidv4(),
+        id: 1,
         name: 'Apple',
         price: 199.99,
         description: 'A delicious apple',
-        imageUrl: `https://picsum.photos/150/150?random=${uuidv4()}`
+        imageUrl: `https://picsum.photos/150/150?random=${1}`,
+        qty: 0,
+        max: 10,
     },
     {
-        id: uuidv4(),
+        id: 2,
         name: 'Banana',
         price: 123.66,
         description: 'A delicious banana',
-        imageUrl: `https://picsum.photos/150/150?random=${uuidv4()}`
+        imageUrl: `https://picsum.photos/150/150?random=${2}`,
+        qty: 0,
+        max: 15,
     },
     {
-        id: uuidv4(),
+        id: 3,
         name: 'Orange',
         price: 5920.69,
         description: 'A delicious orange',
-        imageUrl: `https://picsum.photos/150/150?random=${uuidv4()}`
+        imageUrl: `https://picsum.photos/150/150?random=${3}`,
+        qty: 0,
+        max: 13,
     },
     {
-        id: uuidv4(),
+        id: 4,
         name: 'Strawberry',
         price: 23.33,
         description: 'A delicious strawberry',
-        imageUrl: `https://picsum.photos/150/150?random=${uuidv4()}`
+        imageUrl: `https://picsum.photos/150/150?random=${4}`,
+        qty: 0,
+        max: 50,
     },
     {
-        id: uuidv4(),
+        id: 5,
         name: 'Watermelon',
         price: 1.99,
         description: 'A delicious watermelon',
-        imageUrl: `https://picsum.photos/150/150?random=${uuidv4()}`
+        imageUrl: `https://picsum.photos/150/150?random=${5}`,
+        qty: 0,
+        max: 34,
     },
 ];
 
