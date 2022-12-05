b4afa7c1bfe3faa64c47b2da147254a8b25d5604
Hector Romero <hectorromerodev@gmail.com>
2022-12-05 14:06:48 -0800

    update shopCartPage

--- src/pages/ShopCartPage.jsx
+++ src/pages/ShopCartPage.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 import ProductSummaryItem from '../components/ProductSummaryItem';
 
@@ -11,6 +14,23 @@ import products from "../data/products";
 const ShopCartPage = () => {
     // const { cart } = useContext(CartContext);
     
+    const [cart, setCart] = useState(products);
+
+    const handleQtyChange = ({id, qty}) => {
+        const newCart = cart.map((product) => {
+            if (product.id === id) {
+                return { ...product, qty };
+            }
+            return product;
+        });
+        setCart(newCart);
+    };
+
+    // const total = cart.reduce((acc, product) => {
+    //     return acc + product.price * product.qty;
+    // }, 0);
+
+    
     
 
     return (
@@ -18,7 +38,10 @@ const ShopCartPage = () => {
             <Grid container spacing={2}>
                 <Grid item xs={8}>
                     <Container sx={{
-                        marginTop:"2rem",
+                        margin:"1rem",
+                        boxShadow: "0 0 5px 0 rgba(0,0,0,0.1)",
+                        borderRadius: "5px",
+                        padding: "1rem",
                     }}>
                     
                         <Typography variant="h4" gutterBottom component="div">
@@ -26,9 +49,9 @@ const ShopCartPage = () => {
                         </Typography>
 
                         {
-                            products.map((product) => (
-                                <Box key="product.id">
-                                    <ProductSummaryItem product={product} />
+                            cart.map((p) => (
+                                <Box key={p.id}>
+                                    <ProductSummaryItem product={p} changeQty={handleQtyChange} />
                                 </Box>
                             ))
                         }
